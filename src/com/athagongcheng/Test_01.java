975cb7d13baddc1e449da590b1359baa78566500
gentelyang <852221609@qq.com>
2017-03-10 15:26:11 +0800

    romote update fiel
    good

--- src/com/athagongcheng/Test_01.java
+++ src/com/athagongcheng/Test_01.java
@@ -4,5 +4,6 @@ public class Test_01 {
 	public static void main(String[] args) {
 		System.out.println("Hello github ·þÎñÆ÷");
 		System.out.println("hello liyang ,我是服务器，你好厉害");
+	        System.out.println("Hello liyang ,this is second project");		
 	}
 }
