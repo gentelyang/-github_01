e8f571553dbc1a15cba72619f6251e1b41ab6e1c
gentelyang <852221609@qq.com>
2017-03-10 15:13:30 +0800

    Update Test_01.java

--- src/com/athagongcheng/Test_01.java
+++ src/com/athagongcheng/Test_01.java
@@ -3,6 +3,6 @@ package com.athagongcheng;
 public class Test_01 {
 	public static void main(String[] args) {
 		System.out.println("Hello github ·þÎñÆ÷");
-		System.out.println("hello liyang ,我是服务器，你好厉害！");
+		System.out.println("hello liyang ,我是服务器，你好厉害");
 	}
 }
