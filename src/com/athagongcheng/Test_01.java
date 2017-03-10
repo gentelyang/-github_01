53ed6a09c2f9f7f61b163a50614efc2463daf2eb
gentelyang <852221609@qq.com>
2017-03-10 15:34:26 +0800

    Update Test_01.java

--- src/com/athagongcheng/Test_01.java
+++ src/com/athagongcheng/Test_01.java
@@ -6,5 +6,6 @@ public class Test_01 {
 		System.out.println("hello liyang ,我是服务器，你好厉害");
 	        System.out.println("Hello liyang ,this is second project");	
 	        System.out.println("李洋要进行本地提交了....");
+		System.out.println("你好李洋，i coming too");
 	}
 }
